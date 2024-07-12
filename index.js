6828f700c76b466e226aa2e00e6ef05c574d8b13
titus-cloud <titus.onzere@student.moringaschool.com>
2024-07-12 07:13:57 +0300

    This is my arithmeic lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 0) + 1;
+
+console.log(random);
+
+const num3 = 10;
+const num4 = 6;
+
+const mod = num3 % num4;
+console.log(mod);
+
+const numbers = [1, 8, 16, 20];
+
+// console.log(Math.max(...Number));
+const max = Math.max(...numbers);
